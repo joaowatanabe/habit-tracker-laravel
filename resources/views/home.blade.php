85d5261ddee705f1098f0c9541ebe8c017de2079
wabemusic <jvwatanabe@gmail.com>
2026-07-14 21:48:20 -0300

    feat: setup tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,15 @@
-<div>
-    <h1>Welcome to the Habit Tracker</h1>
-    <p>Olá, {{ $name }}!</p>
-    <p>Seus Hábitos:</p>
-    <ul>
-        @foreach ($habits as $habit)
-            <li>{{ $habit }}</li>
-        @endforeach
-    </ul>
-</div>
-
-@auth
-    <p>Você está logado</p>
-@endauth
-
-@guest
-    <p>Você não está logado!</p>
-@endguest
+<!doctype html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>
+      {{ config('app.name', 'Habit Tracker') }}
+    </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    @vite('resources/css/app.css')
+  </head>
+  <body>
+    <h1 class="text-3xl font-bold underline">Hello World!</h1>
+  </body>
+</html>
