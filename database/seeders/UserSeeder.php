6887a20caafac9ab519eb3626770dc183599ed9f
wabemusic <jvwatanabe@gmail.com>
2026-07-21 18:01:12 -0300

    feat(Auth): add login functionality and user session management

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::query()->create([
+            'name' => 'Fulano de Tal',
+            'email' => 'fulano@gmail.com',
+            'password' => '123456'
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 use App\Http\Controllers\SiteController;
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
-
+// SITE
 Route::get('/', [SiteController::class, 'index']);
 
+// LOGIN
+Route::get('/login', [LoginController::class, 'index']);
